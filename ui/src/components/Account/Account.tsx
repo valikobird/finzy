b720cb44ceace77591d63820932b57ebf637714a
Valentyn Zozulia <valikobird@gmail.com>
2021-08-18 22:18:36 -0500

    UI: Add fetching a single account summary

--- ui/src/components/Account/Account.tsx
+++ ui/src/components/Account/Account.tsx
@@ -1,25 +1,36 @@
-import React, {Fragment, useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import {IAccount, IAccountTransactionHistory} from "../../interfaces";
+import React, { Fragment, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IAccount, IAccountTransactionHistory } from "../../interfaces";
 import AccountSummary from "./components/AccountSummary";
 import currencyFormatter from "../../shared/currencyFormatter";
 import AccountDiffs from "./components/AccountDiffs";
 import AccountTransactions from "./components/AccountTransactions";
 
 export default function Account() {
-  const {id} = useParams<{ id: string }>();
+  const { id } = useParams<{ id: string }>();
 
   const [account, setAccount] = useState<IAccount>();
   const [balance, setBalance] = useState<IAccountTransactionHistory>();
   const [formatter, setFormatter] = useState<any>();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    setAccount({
-      id,
-      title: "Account Title",
-      currency: "USD",
-      balance: 1234,
-    });
+    fetch(`http://localhost:4000/v1/account/${id}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          let err = Error(`Invalid response code: ${response.status}`);
+          setError(err);
+          return { account: null };
+        } else {
+          error && setError(null);
+          return response.json();
+        }
+      })
+      .then((json) => {
+        setAccount(json.account);
+        setIsLoaded(true);
+      });
 
     setBalance({
       diffByPeriod: {
@@ -34,35 +45,49 @@ export default function Account() {
           timestamp: new Date(),
           amount: 1234,
           type: "Deposit",
-        }, {
+        },
+        {
           id: "2",
           timestamp: new Date(),
           amount: -123,
           type: "Dividend",
-        }
+        },
       ],
-    })
+    });
   }, []);
 
   useEffect(() => {
     account && setFormatter(currencyFormatter(account.currency));
   }, [account]);
 
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <Fragment>
-      <h2>Account {account?.id}: {account?.title}</h2>
+      <h2>
+        {account?.title} ({account?.currency})
+      </h2>
 
-      {account && formatter
-        ? <AccountSummary account={account} formatter={formatter}/>
-        : null}
+      {account && formatter ? (
+        <AccountSummary account={account} formatter={formatter} />
+      ) : null}
 
-      {balance && formatter
-        ? <AccountDiffs diffs={balance.diffByPeriod} formatter={formatter}/>
-        : null}
+      {balance && formatter ? (
+        <AccountDiffs diffs={balance.diffByPeriod} formatter={formatter} />
+      ) : null}
 
-      {balance && formatter
-        ? <AccountTransactions transactions={balance.transactions} formatter={formatter}/>
-        : null}
+      {balance && formatter ? (
+        <AccountTransactions
+          transactions={balance.transactions}
+          formatter={formatter}
+        />
+      ) : null}
     </Fragment>
   );
 }
