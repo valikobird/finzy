f002d186a254e69e6237d8e57da6fa823f875437
Valentyn Zozulia <valikobird@gmail.com>
2021-08-18 21:53:16 -0500

    UI: Add fetching all accounts

--- ui/src/components/Accounts.tsx
+++ ui/src/components/Accounts.tsx
@@ -1,18 +1,38 @@
-import React, {Fragment, useEffect, useState} from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {IAccount} from "../interfaces";
+import { IAccount } from "../interfaces";
 import currencyFormatter from "../shared/currencyFormatter";
 
 export default function Accounts() {
-
   const [accounts, setAccounts] = useState<Array<IAccount>>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    setAccounts([
-      {id: "1", title: "Cash", currency: "CLP", balance: 16000000.00},
-      {id: "2", title: "Payoneer", currency: "USD", balance: 10000.00},
-    ]);
+    fetch("http://localhost:4000/v1/accounts")
+      .then((response) => {
+        if (response.status !== 200) {
+          let err = Error(`Invalid response code: ${response.status}`);
+          setError(err);
+          return { accounts: [] };
+        } else {
+          error && setError(null);
+          return response.json();
+        }
+      })
+      .then((json) => {
+        setAccounts(json.accounts);
+        setIsLoaded(true);
+      });
   }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <Fragment>
@@ -20,24 +40,26 @@ export default function Accounts() {
 
       <table className="table table-hover">
         <thead>
-        <tr>
-          <th scope="col">Title</th>
-          <th scope="col">Currency</th>
-          <th scope="col">Balance</th>
-        </tr>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Currency</th>
+            <th scope="col">Balance</th>
+          </tr>
         </thead>
         <tbody>
-        {accounts.map(account => (
-          <tr key={account.id}>
-            <td>
-              <Link to={`/accounts/${account.id}`}>{account.title}</Link>
-            </td>
-            <td>{account.currency}</td>
-            <td>{currencyFormatter(account.currency).format(account.balance)}</td>
-          </tr>
-        ))}
+          {accounts.map((account) => (
+            <tr key={account.id}>
+              <td>
+                <Link to={`/accounts/${account.id}`}>{account.title}</Link>
+              </td>
+              <td>{account.currency}</td>
+              <td>
+                {currencyFormatter(account.currency).format(account.balance)}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Fragment>
   );
-};
+}
